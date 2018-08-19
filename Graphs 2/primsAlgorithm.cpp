53becadf000f6bed45ad74047734c036a373985d
LavishGulati <lavish00gulati@gmail.com>
2018-08-20 01:14:21 +0530

    Added solutions for Graphs 2

--- Graphs 2/primsAlgorithm.cpp
+++ Graphs 2/primsAlgorithm.cpp
@@ -1,49 +1,64 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int v, e;
-    cin >> v >> e;
-
-    int **input = new int*[v];
-    for(int i = 0; i < v; i++){
-        input[i] = new int[v];
-        for(int j = 0; j < v; j++) input[i][j] = -1;
+int findMinVertex(int *weight, bool *isVisited, int n){
+    int minVertex = -1;
+    for(int i = 0; i < n; i++){
+        if(!isVisited[i] && (minVertex == -1 || weight[i] < weight[minVertex])){
+            minVertex = i;
+        }
     }
 
-    int ei, ej, w;
+    return minVertex;
+}
+
+int main(){
+    int n, e;
+    cin >> n >> e;
+
+    int **input = new int*[n];
+    for(int i = 0; i < n; i++){
+        input[i] = new int[n];
+        for(int j = 0; j < n; j++) input[i][j] = -1;
+    }
+
     for(int i = 0; i < e; i++){
+        int ei, ej, w;
         cin >> ei >> ej >> w;
         input[ei][ej] = w;
         input[ej][ei] = w;
     }
 
-    int *parent = new int[v];
-    int *weight = new int[v];
-    for(int i = 0; i < v; i++) weight[i] = INT_MAX;
+    int *parent = new int[n];
+    int *weight = new int[n];
+    bool *isVisited = new bool[n];
+    for(int i = 0; i < n; i++){
+        weight[i] = INT_MAX;
+        isVisited[i] = false;
+    }
 
-    bool *isVisited = new bool[v];
-    for(int i = 0; i < v; i++) isVisited[i] = false;
+    parent[0] = -1;
+    weight[0] = 0;
 
-    for(int i = 0; i < v-1; i++){
-        isVisited[i] = true;
+    for(int i = 0; i < n; i++){
+        int minVertex = findMinVertex(weight, isVisited, n);
+        isVisited[minVertex] = true;
 
-        for(int j = 0; j < v; j++){
-            if(input[i][j] != -1 && !isVisited[j]){
-                if(input[i][j] < weight[j]){
-                    parent[j] = i;
-                    weight[j] = input[i][j];
+        for(int j = 0; j < n; j++){
+            if(input[minVertex][j] > 0 && !isVisited[j]){
+                if(input[minVertex][j] < weight[j]){
+                    weight[j] = input[minVertex][j];
+                    parent[j] = minVertex;
                 }
             }
         }
     }
 
-    for(int i = 1; i < v; i++){
-        ei = i;
-        ej = parent[i];
-        w = weight[i];
-
-        if(ei <= ej) cout << ei << " " << ej << " " << w << endl;
+    for(int i = 1; i < n; i++){
+        int ei = i;
+        int ej = parent[i];
+        int w = weight[i];
+        if(ei < ej) cout << ei << " " << ej << " " << w << endl;
         else cout << ej << " " << ei << " " << w << endl;
     }
 }
